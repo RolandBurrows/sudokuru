c450797531289dc06db33802046d425173715cba
Roland Burrows <rolandnburrows@gmail.com>
2015-12-26 23:59:08 -0700

    Complete execution basic milestones
    Woot!

--- sudokuru.rb
+++ sudokuru.rb
@@ -3,3 +3,19 @@
 
 puts ""
 puts "Let's solve some Sudoku!"
+
+if ARGV[0] != nil
+	@input_file = ARGV[0]
+else
+	puts "No file specified. Searching for default input file."
+	@input_file = "./puzzles/input.txt"
+end
+
+begin
+	file = File.open(@input_file, "r")
+rescue
+	puts "ERROR. Given file doesn't exist. Halting."
+	exit
+end
+data = file.read
+file.close
