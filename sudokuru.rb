5442fa0ac4f485715c44dc339c7789783d9e3979
Roland Burrows <rolandnburrows@gmail.com>
2015-12-28 19:05:01 -0700

    Downgrade instance variable to local
    Learn your place, local variable!

--- sudokuru.rb
+++ sudokuru.rb
@@ -5,18 +5,18 @@ puts ""
 puts "Let's solve some Sudoku!"
 
 if ARGV[0] != nil
-	@input_file = ARGV[0]
+	input_file = ARGV[0]
 else
 	puts "No file specified. Searching for default input file."
-	@input_file = "./puzzles/input.txt"
+	input_file = "./puzzles/input.txt"
 end
 
 begin
-	puts "Using provided file: #{@input_file}"
-	file = File.open(@input_file, "r")
+	puts "Using provided file: #{input_file}"
+	file = File.open(input_file, "r")
 rescue
-	puts "ERROR. Given file doesn't exist. Halting."
 	exit
 end
+
 data = file.read
 file.close
