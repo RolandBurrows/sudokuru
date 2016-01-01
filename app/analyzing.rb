29f551a8e2d06c703448183f26244d74d1aa4b36
Roland Burrows <rolandnburrows@gmail.com>
2016-01-01 14:53:36 -0700

    Data analysis milestone reached
    Boom, baby.

--- app/analyzing.rb
+++ app/analyzing.rb
@@ -1,13 +1,23 @@
+require_relative "logging"
+
 class Analyze
 	class << self
 
 		def data_formatting(file_data)
+			
 			lines = file_data.split("\n")
+
 			lines.each { |line|
-				line.split("").each do |i|
-  				puts i
-				end
+				line.split("").each { |char|
+	  			if (char.match("[1-9]| |-|_") != nil)
+	  				# Pass!
+	  			else
+	  				Log.error("Row (#{line}) contains a formatting issue. Please fix and rerun.")
+	  			end
+				}
 			}
+
+			Log.info("Puzzle data is properly formatted.")
 		end
 
 	end
