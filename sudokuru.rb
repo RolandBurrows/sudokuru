fe055f42139aadbe987327e9fc4898a609c79b67
Roland Burrows <rolandnburrows@gmail.com>
2016-01-05 21:18:23 -0700

    Add comments to growing primary script
    Too big to fail.

--- sudokuru.rb
+++ sudokuru.rb
@@ -5,12 +5,16 @@ Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| load(f) }
 
 Log.info("Let's solve some Sudoku!")
 
+# Initialize test file from the user or provide a default
+
 if ARGV[0] != nil
 	input_file = ARGV[0]
 else
 	Log.info("No file specified. Searching for default input file.")
 	input_file = "./puzzles/input.txt"
 end
+
+# Extract file data or warn user of non-existences
 
 begin
 	Log.info("Using provided file: #{input_file}")
@@ -25,10 +29,16 @@ rescue
 	Log.error("given file doesn't exist. Halting.")
 end
 
+# Display file contents
+
 Log.info("File contents:")
 Log.tab(puzzle_data)
 
+# Analyze file contents
+
 Analyze.data_formatting(puzzle_data)
 Analyze.dimensionality(puzzle_data)
+
+# Cleanup
 
 puzzle_file.close
