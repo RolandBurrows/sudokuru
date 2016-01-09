07d92ec6c67cbcecf61bde6acc8403efa2c2bd32
Roland Burrows <rolandnburrows@gmail.com>
2016-01-09 13:09:43 -0700

    Increase code readability
    Silly rabbit, typecasting is for kids.

--- app/analyze.rb
+++ app/analyze.rb
@@ -4,16 +4,17 @@ require "Matrix"
 class Analyze
 
 	def initialize(file_data)
-		@raw_data = file_data
 		@rows = file_data.split("\n")
 
-		matrix_data = Matrix[]
+		@matrix_data = Matrix[]
+
 		@rows.each { |line|
-			matrix_data = Matrix.rows(matrix_data.to_a << (line.split("")))
+			@matrix_data = Matrix.rows(@matrix_data.to_a << (line.split("")))
 		}
+
 		@columns = []
-		for i in 1..(matrix_data.count)
-			@columns.push(matrix_data.column(i-1).to_a)
+		for i in 1..(@matrix_data.count)
+			@columns.push(@matrix_data.column(i-1).to_a)
 		end
 	end
 
@@ -27,7 +28,6 @@ class Analyze
   			end
 			}
 		}
-
 		Log.info("Puzzle data is properly formatted.")
 	end
 
@@ -40,16 +40,15 @@ class Analyze
 				Log.error("Row (#{line}) length does not match column height. Please fix and rerun.")
 			end
 		}
-
 		Log.info("Puzzle height and length are equivalent.")
 	end
 
 	def row_uniqueness
 		@rows.each { |line|
 			for i in 1..9
-				if line.count(i.to_s).to_s == "1"
+				if line.count(i.to_s) == 1
 					# Pass!
-				elsif line.count(i.to_s).to_s == "0"
+				elsif line.count(i.to_s) == 0
 					# Pass!
 				else
 					Log.error("Row (#{line}) contains duplicate values. Please fix and rerun.")
@@ -63,9 +62,9 @@ class Analyze
 	def column_uniqueness
 		@columns.each { |vertical|
 			for i in 1..9
-				if vertical.count(i.to_s).to_s == "1"
+				if vertical.count(i.to_s) == 1
 					# Pass!
-				elsif vertical.count(i.to_s).to_s == "0"
+				elsif vertical.count(i.to_s) == 0
 					# Pass!
 				else
 					vertical = vertical*""   # converts the array to a solid string for better readability
