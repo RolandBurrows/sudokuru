61ddc1dc477e631d1d5b28d16578dea46e3b6e68
Roland Burrows <rolandnburrows@gmail.com>
2016-01-20 21:47:55 -0700

    Return the best starting slice
    Bam!

--- app/solve.rb
+++ app/solve.rb
@@ -13,24 +13,49 @@ class Solve
 	end
 
 	def find_starting_slice
-		determine_most_filled_incomplete_row
+		best_row = determine_most_filled_incomplete_row(@pure_puzzle_data)
+		best_column = determine_most_filled_incomplete_column(@pure_puzzle_data)
+		if best_row[1] >= best_column[1]
+			puts best_row.push("row")
+			return best_row.push("row")
+		else
+			puts best_column.push("column")
+			return best_column.push("column")
+		end
 	end
 
-	def determine_most_filled_incomplete_row
+	def determine_most_filled_incomplete_row(matrix_formatted_data)
 		row_counts = []
-		@pure_puzzle_data.row_vectors.each { |row|
+		matrix_formatted_data.row_vectors.each { |row|
 			row_digits = 0
 			row.each { |char|
 				if char.match("[1-9]")
 					row_digits += 1
 				end
 			}
-			if row_digits == @pure_puzzle_data.row(0).count
+			if row_digits == matrix_formatted_data.row(0).count
 				row_digits = 0
 			end
 			row_counts.push(row_digits)
 		}
 		return [row_counts.index(row_counts.max), row_counts.max]
+	end
+
+		def determine_most_filled_incomplete_column(matrix_formatted_data)
+		column_counts = []
+		matrix_formatted_data.column_vectors.each { |column|
+			column_digits = 0
+			column.each { |char|
+				if char.match("[1-9]")
+					column_digits += 1
+				end
+			}
+			if column_digits == matrix_formatted_data.row(0).count
+				column_digits = 0
+			end
+			column_counts.push(column_digits)
+		}
+		return [column_counts.index(column_counts.max), column_counts.max]
 	end
 
 end
