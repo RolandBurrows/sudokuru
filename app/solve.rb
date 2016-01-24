98fd6327bb134fd94b0d8f6284c5a9f21bfc0a70
Roland Burrows <rolandnburrows@gmail.com>
2016-01-24 10:44:36 -0700

    Set up array-to-matrix method
    Because matrices are immutable. ಠ_ಠ.

--- app/solve.rb
+++ app/solve.rb
@@ -10,7 +10,9 @@ class Solve
 	end
 
 	def populate_naked_singles
+		index_counter = (-1)
 		@pure_puzzle_data.row_vectors.each { |row|
+			index_counter += 1
 			spaces = row.count(" ")
 			dashes = row.count("-")
 			underscores = row.count("_")
@@ -23,10 +25,23 @@ class Solve
 			row_array.delete("-")
 			row_array.delete("_")
 
-			missing_digit = (string_digits - row_array)
+			missing_digits = (string_digits - row_array)
+
+			if (missing_digits.length == 1)
+				
+			end
 		}
 
 
+	end
+
+	def convert_array_back_to_matrix(array_of_arrays)
+		matrix_data = Matrix[]
+
+		array_of_arrays.each { |row|
+			matrix_data = Matrix.rows(matrix_data.to_a << row)
+		}
+		return matrix_data
 	end
 
 end
