5f9969c2040217ee1bf5c00de46a42b4f4e18a41
Roland Burrows <rolandnburrows@gmail.com>
2016-02-04 00:15:09 -0700

    Add matrix cell filler
    It's a very selective, discerning sort of method.

--- app/loop.rb
+++ app/loop.rb
@@ -9,6 +9,7 @@ class Loop
 		@pure_puzzle_array = matrix.to_a
 		@matrix_data = matrix
 		@solver = Solve.new(@pure_puzzle_matrix)
+		@modified_matrix = Matrix[]
 	end
 
 	def fill_in_naked_singles
@@ -30,9 +31,28 @@ class Loop
 		determinant = Determine.new(@matrix_data)
 		possible_digits_formatted = determinant.determine_all_possible_digits_per_cell
 		possible_digits_matrix = @solver.convert_array_back_to_matrix(possible_digits_formatted)
-		point = determinant.find_starting_point
+		start_point = determinant.find_starting_point
 
 		return @matrix_data
+	end
+
+
+	private
+
+	def fill_matrix_cell_with_value(matrix_data, cell_index, replacement_value)
+		data_array = matrix_data.to_a
+		data_row = data_array[cell_index[0]]
+		existing_value = data_row[cell_index[1]]
+
+		if existing_value !~ /( |-|_)/
+			raise "Cannot replace a non-blank value!"
+		end
+
+		data_row[cell_index[1]] = replacement_value
+		data_array[cell_index[0]] = data_row
+
+		@modified_matrix = @solver.convert_array_back_to_matrix(data_array)
+		return @modified_matrix
 	end
 
 end
