0ce13567553c1e2715f0c8f3136ba3ca6a44bab7
Roland Burrows <rolandnburrows@gmail.com>
2016-02-26 22:50:58 -0700

    Log error if puzzle has no solution
    Solving at the speed of light! Unless the puzzle is a black hole.

--- app/loop.rb
+++ app/loop.rb
@@ -13,32 +13,41 @@ class Loop
 		last_move = nil
 		end_time = Time.now + 60
 
-		while Time.now < end_time
-			all_possibilities = determinant.determine_all_possible_digits_per_cell
-			replacement_row = all_possibilities[start_point[0]]
-			replacement_options = replacement_row[start_point[1]]
+		begin
+			while Time.now < end_time
+				all_possibilities = determinant.determine_all_possible_digits_per_cell
+				replacement_row = all_possibilities[start_point[0]]
+				replacement_options = replacement_row[start_point[1]]
 
-			if last_move != nil
-				iter = replacement_options.index(last_move)
-				replacement_options = replacement_options[iter+1, replacement_options.size]
-				last_move = nil
+				if last_move != nil
+					iter = replacement_options.index(last_move)
+					replacement_options = replacement_options[iter+1, replacement_options.size]
+					last_move = nil
+				end
+
+				move = choose_move(replacement_options)
+
+				if move == nil
+					# Backtrack due to illegal move
+					start_point, last_move = @state.pop_state
+				else
+					@state[start_point[0], start_point[1]] = move
+					# Use Determine initializer to break if success
+					determinant = Determine.new(@state.board, @pure_boxmap_matrix)
+					start_point = determinant.find_starting_point
+					last_move = nil
+				end
+
+				if Time.now >= end_time
+					Log.error("Solving the puzzle took longer than 60 seconds. Please reduce puzzle size or diagnose with 'env DEBUG='yes'.")
+				end
 			end
 
-			move = choose_move(replacement_options)
-
-			if move == nil
-				# Backtrack due to illegal move
-				start_point, last_move = @state.pop_state
+		rescue
+			if $box_map_used
+				Log.error("Puzzle + box map combination has no solution.")
 			else
-				@state[start_point[0], start_point[1]] = move
-				# Use Determine initializer to break if success
-				determinant = Determine.new(@state.board, @pure_boxmap_matrix)
-				start_point = determinant.find_starting_point
-				last_move = nil
-			end
-
-			if Time.now >= end_time
-				Log.error("Solving the puzzle took longer than 60 seconds. Please reduce puzzle size or diagnose with 'env DEBUG='yes'.")
+				Log.error("Puzzle has no solution.")
 			end
 		end
 
