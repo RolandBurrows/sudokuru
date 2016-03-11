5a3ed12e418736fa6096618aeea95ede54e434b9
Roland Burrows <rolandnburrows@gmail.com>
2016-03-10 22:31:00 -0700

    Methodize the initialize
    :collision: :mag: :package: :page_facing_up: :loop:

--- app/sudoku.rb
+++ app/sudoku.rb
@@ -1,61 +1,67 @@
 class Sudoku
 
   def initialize(puzzle_file, boxmap_file)
-
     file_handler = FileHandler.new(puzzle_file, boxmap_file)
-    puzzle_data = file_handler.extract_puzzle_data_from_file
-    box_map_data = file_handler.extract_boxmap_data_from_file
+    @puzzle_data = file_handler.extract_puzzle_data_from_file
+    @box_map_data = file_handler.extract_boxmap_data_from_file
+    @matrix_puzzle_data = nil
+    @matrix_box_data = nil
 
-    # Convert file contents and display
-
-    converter = Transmute.new()
-
-    begin
-      @matrix_puzzle_data = converter.convert_file_data_to_matrix(puzzle_data)
-      @matrix_box_data = converter.convert_file_data_to_matrix(box_map_data) if box_map_data
-    rescue
-      Log.error("Puzzle/BoxMap file rows and/or columns need to be of consistent length. Please fix and rerun.")
-    end
+    convert_files_to_matrices
 
     Log.display_raw_data(@matrix_puzzle_data, "Puzzle")
     Log.display_raw_data(@matrix_box_data, "Box Map") if @matrix_box_data
 
-    # Analyze puzzle file contents
+    analyze_puzzle_file_contents
 
-    analysis = Analyze.new(@matrix_puzzle_data, @matrix_box_data)
+    analyze_box_file_contents
+
+    # Format puzzle
+    transmute = Transmute.new(@matrix_puzzle_data)
+    @puzzle_matrix = transmute.standardize_blanks
+
+    solve_it
+  end
+
+  def convert_files_to_matrices
+    converter = Transmute.new()
+    begin
+      @matrix_puzzle_data = converter.convert_file_data_to_matrix(@puzzle_data)
+      @matrix_box_data = converter.convert_file_data_to_matrix(@box_map_data) if @box_map_data
+    rescue
+      Log.error("Puzzle/BoxMap file rows and/or columns need to be of consistent length. Please fix and rerun.")
+    end
+  end
+
+  def analyze_puzzle_file_contents
+  	analysis = Analyze.new(@matrix_puzzle_data, @matrix_box_data)
     analysis.dimensionality
     analysis.data_formatting
     analysis.row_uniqueness("log it!")
     analysis.column_uniqueness("log it!")
+  end
 
-    # Analyze box map file contents
-
-    if @matrix_box_data
+  def analyze_box_file_contents
+  	if @matrix_box_data
       boxysis = BoxHandler.new(@matrix_puzzle_data, @matrix_box_data)
       boxysis.dimensionality(@matrix_puzzle_data)
       boxysis.data_formatting
       boxysis.data_uniqueness
       boxysis.box_uniqueness("log it!")
     end
+  end
 
-    # Format puzzle
-
-    transmute = Transmute.new(@matrix_puzzle_data)
-    @puzzle_matrix = transmute.standardize_blanks
-
-    # Solve!
-
-    looper = Loop.new(@puzzle_matrix, @matrix_box_data)
+  def solve_it
+  	looper = Loop.new(@puzzle_matrix, @matrix_box_data)
     begin
       looper.fill_puzzle
     rescue
-      if box_map_data
+      if @box_map_data
         Log.error("Puzzle + box map combination has no solution.")
       else
         Log.error("Puzzle has no solution.")
       end
     end
-
   end
 
 end
