9efaa2fc1ce3c8305a379dcfa3fabc0bfd3d4f89
Roland Burrows <rolandnburrows@gmail.com>
2016-03-19 19:38:47 -0600

    Complete box handler spec coverage
    Dial coverage up to 11.

---
+++ tests/test_unit_box_handler.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+require 'minitest/autorun'
+require "matrix"
+
+describe "sudokuru_unit_box_handler" do
+
+  it "should replace non erroneous box values with blanks" do
+    load "././app/box_handler.rb"
+    box_map = Matrix.identity(3)
+    puzzle = Matrix.zero(3)
+    box_handler = BoxHandler.new(puzzle, box_map)
+    value = 1
+    output = box_handler.replace_non_erroneous_box_values_with_blanks(box_map, value)
+    output.must_equal (Matrix[[1, "-", "-"], ["-", 1, "-"], ["-", "-", 1]])
+  end
+
+end
